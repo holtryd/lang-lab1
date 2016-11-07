79bb42a153ed8cc90fcb189bd36a606a9c828a89
Nicklas Ramhöj <nicklas@elabs.se>
2016-11-07 19:45:36 +0100

    Adding non-working c example

--- examples/comment/source.cc
+++ examples/comment/source.cc
@@ -1,1 +1,21 @@
-// this is just a comment
+// a fibonacci function showing most features of the CMM language
+
+int mx ()
+{
+  return 5000000 ;
+}
+
+int main ()
+{
+  int lo ;
+  int hi ;
+  lo = 1 ;
+  hi = lo ;
+  printf("%d",lo) ;
+  while (hi < mx()) {
+    printf("%d",hi) ;
+    hi = lo + hi ;
+    lo = hi - lo ;
+  }
+  return 0 ;
+}
